7616ac77b9bc0300de2ece0e3e686cf0c2f65fe7
Luca Stefani <luca020400@lineageos.org>
2017-08-24 19:22:46 +0200

    hydrogen: Add rild socket shim
    Change-Id: Ia5aa2cf867678045215625f6297708db6f49fdaa

--- device.mk
+++ device.mk
@@ -48,10 +48,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl:system/usr/keylayout/msm8976-tashalite-snd-card_Button_Jack.kl
 
-# Libshims
+# Shims
 PRODUCT_PACKAGES += \
     libshims_ims \
-    libshims_get_process_name
+    libshims_get_process_name \
+    rild_socket
 
 # Ramdisk
 PRODUCT_PACKAGES += \
