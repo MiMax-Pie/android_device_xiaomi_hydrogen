6b221614dba9e6b6a3bf0807273c37f8d457073f
TARKZiM <tom8476oo@gmail.com>
2019-01-03 20:13:42 +0800

    hydrogen: update build fingerprint to V10.1.1.0.NBCMIFI

--- lineage_hydrogen.mk
+++ lineage_hydrogen.mk
@@ -33,5 +33,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="hydrogen-user 7.0 NRD90M V9.6.2.0.NBCMIFD release-keys"
-BUILD_FINGERPRINT=Xiaomi/hydrogen/hydrogen:7.0/NRD90M/V9.6.2.0.NBCMIFD:user/release-keys
+    PRIVATE_BUILD_DESC="hydrogen-user 7.0 NRD90M V10.1.1.0.NBCMIFI release-keys"
+    BUILD_FINGERPRINT=Xiaomi/hydrogen/hydrogen:7.0/NRD90M/V10.1.1.0.NBCMIFI:user/release-keys
